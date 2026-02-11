f2d2be0edebe88d9682bc2f65e0347e371c0b105
Sai Madhav <madhav200320@gmail.com>
2026-02-11 19:50:12 +0530

    Fix Login 500 Error: Add missing cloudinary dependency and config vars

--- api/config.py
+++ api/config.py
@@ -6,3 +6,7 @@ load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 DB_NAME = os.getenv("DB_NAME", "mindrise")
+
+CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
